a81b42684a923665ef48ef5f9c22b24fe26e6377
Danladi Etu <danladietugithub@gmail.com>
2023-03-30 14:42:59 -0700

    task0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Creates a new binary tree node
+ * @parent: Pointer to the parent node to create
+ * @value: value of the node to be create
+ *
+ * Return: Pointer to the newnode or NULL on failure
+ *
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode;
+
+	/* Alloocate memory space to the node */
+	newNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+
+	if (!newNode)
+		return (NULL);
+
+	newNode->parent = parent;
+	newNode->n = value;
+	newnode->left= NULL;
+	newNode->right = NULL;
+
+	/*return a pointer to the node */
+
+	return (newNode);
+}
