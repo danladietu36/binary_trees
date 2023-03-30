1432544ed38fc9044e453976f7905dc797364214
Danladi Etu <danladietugithub@gmail.com>
2023-03-30 16:31:30 -0700

    task1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,10 +1,12 @@
-#include "binary_tree.h"
+#include "binary_trees.h"
 
 /**
  * binary_tree_insert_left - inserts a node as the left child of another node
  * @parent: Pointer to the parent of the node to be inserted
  * @value: value to store in the new node
- *
+ * Description - If parent already has a left-child, the new node must
+ *				take its place, and the old left-child must be set as
+ *				the left-child of the new node.
  * Return: Pointer to the new node created ot NULL on fairlure.
  *
  */
