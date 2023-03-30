bbc145dca7471e85d9d35b56b8b08b5ddcf8e840
Danladi Etu <danladietugithub@gmail.com>
2023-03-30 16:46:15 -0700

    task1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,7 +16,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *newNode;
 
 	/* Alloacte memory space for the new node */
-	newNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	newNode = malloc(sizeof(binary_tree_t));
 
 	/* Checks if parent exist */
 
