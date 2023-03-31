5f02d97c76f3523e04f78ce1596dd1330379a610
Danladi Etu <danladietugithub@gmail.com>
2023-03-30 18:05:07 -0700

    task3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -11,7 +11,7 @@ void binary_tree_delete(binary_tree_t *tree)
 {
 	if (!tree)
 		return;
-	binary_tree_delete(tree->right);	
+	binary_tree_delete(tree->right);
 	binary_tree_delete(tree->left);
 	free(tree);
 }
