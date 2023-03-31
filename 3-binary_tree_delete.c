da4a0635a8393484c000a077e4f48a7552a6dbf9
Danladi Etu <danladietugithub@gmail.com>
2023-03-30 18:15:24 -0700

    task3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * void binary_tree_delete - Function to delete an entire binary tree
+ * binary_tree_delete - Function to delete an entire binary tree
  * @tree: Pointer to the root node of the tree to delete
  *
  * Description: If tree is NULL, do nothing
@@ -11,7 +11,10 @@ void binary_tree_delete(binary_tree_t *tree)
 {
 	if (!tree)
 		return;
-	binary_tree_delete(tree->right);
-	binary_tree_delete(tree->left);
-	free(tree);
+	if (tree)
+	{
+		binary_tree_delete(tree->right);
+		binary_tree_delete(tree->left);
+		free(tree);
+	}
 }
