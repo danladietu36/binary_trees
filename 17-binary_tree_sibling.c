3f70aebd4af4b7f0c86b4152c1f3cc9290ef5d6e
Danladi Etu <danladietugithub@gmail.com>
2023-03-31 16:15:57 -0700

    checks_sibblings

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -14,7 +14,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (!node || !node->parent)
 		return (NULL);
 
-	if (node == node->parent-left)
-		return (node->parent-right);
+	if (node == node->parent->left)
+		return (node->parent->right);
 	return (node->parent->left);
 }
